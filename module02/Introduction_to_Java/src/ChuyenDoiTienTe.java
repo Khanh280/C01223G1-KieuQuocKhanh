44b1047719c981189d4522e76a4447a6fc379272
Khanh280 <Khanh280>
2023-03-05 19:57:20 +0700

    Bai tap

---
+++ module02/Introduction_to_Java/src/ChuyenDoiTienTe.java
@@ -0,0 +1,31 @@
+package Introduction_to_Java.src;
+
+import java.util.Scanner;
+
+public class ChuyenDoiTienTe {
+    public static void main(String[] args) {
+        System.out.print("1.VND -> USD \n2.USD -> VND\n Hãy chọn đơn vị muốn chuyển đổi: ");
+        Scanner sc = new Scanner(System.in);
+        int rate = 23000;
+        boolean flag;
+        do {
+            int loaiTien = sc.nextInt();
+            flag = true;
+            switch (loaiTien) {
+                case 1:
+                    System.out.print("Nhập số tiền VND: ");
+                    float vnd = sc.nextFloat();
+                    System.out.println(vnd + " VND = " + (vnd / rate) + " USD");
+                    break;
+                case 2:
+                    System.out.print("Nhập số tiền USD: ");
+                    float usd = sc.nextFloat();
+                    System.out.println(usd + " USD = " + (usd * rate) + " VND");
+                    break;
+                default:
+                    flag = false;
+                    System.out.println("Vui lòng chọn đúng đơn vị muốn chuyển đổi: ");
+            }
+        } while (!flag);
+    }
+}
---
+++ module02/Introduction_to_Java/src/NumberToString.java
@@ -0,0 +1,89 @@
+package Introduction_to_Java.src;
+
+import java.util.Scanner;
+
+public class NumberToString {
+    public static void main(String[] args) {
+        System.out.print("Nhap so can doc: ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        String result = " ";
+        String teen = "teen";
+        String ty = "ty";
+
+        if (number > 0) {
+            if (number <= 10) {
+                switch (number) {
+                    case 1:
+                        result = "One";
+                        break;
+                    case 2:
+                        result = "Two";
+                        break;
+                    case 3:
+                        result = "Three";
+                        break;
+                    case 4:
+                        result = "Four";
+                        break;
+                    case 5:
+                        result = "Five";
+                        break;
+                    case 6:
+                        result = "Six";
+                        break;
+                    case 7:
+                        result = " Seven";
+                        break;
+                    case 8:
+                        result = "Eight";
+                        break;
+                    case 9:
+                        result = "Nine";
+                        break;
+                    case 10:
+                        result = " Ten";
+                        break;
+                }
+            } else if (number < 20) {
+                switch (number) {
+
+                    case 11:
+                        result = " Eleven";
+                        break;
+                    case 12:
+                        result = "Twelve";
+                        break;
+                    case 13:
+                        result = " Three " + teen;
+                        break;
+                    case 14:
+                        result = " Four " + teen;
+                        break;
+                    case 15:
+                        result = " Five " + teen;
+                        break;
+                    case 16:
+                        result = " Six " + teen;
+                        break;
+                    case 17:
+                        result = " Seven " + teen;
+                        break;
+                    case 18:
+                        result = " Eight " + teen;
+                        break;
+                    case 19:
+                        result = " Nine " + teen;
+                        break;
+                }
+            } else if (number < 100) {
+                switch (number){
+
+                }
+            }
+        }
+
+
+        System.out.println(result);
+    }
+}
