f3d7466be2036b6757d1cf0c69f639691134597b
Khanh280 <Khanh280>
2023-03-06 15:35:56 +0700

    bai tap

--- module02/src/introduction_to_java/bai_tap/NumberToString.java
+++ module02/src/introduction_to_java/bai_tap/NumberToString.java
@@ -4,86 +4,259 @@ import java.util.Scanner;
 
 public class NumberToString {
     public static void main(String[] args) {
-        System.out.print("Nhap so can doc: ");
-        Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        String result = " ";
-        String teen = "teen";
-        String ty = "ty";
 
-        if (number > 0) {
-            if (number <= 10) {
-                switch (number) {
+        boolean flag = true;
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Nhập số để đọc ");
+            int a = sc.nextInt();
+            String zero = "Zero";
+            String one = "One";
+            String two = "Two";
+            String three = "Three";
+            String four = "Four";
+            String five = "Five";
+            String six = "Six";
+            String seven = "Seven";
+            String eight = "Eight";
+            String nine = "Nine";
+            String ten = "Ten";
+
+            String twenty = "twenty ";
+            String thirty = "Thirty ";
+            String forty = "Forty ";
+            String fifty = "Fifty ";
+            String sixty = "Sixty ";
+            String seventy = "Seventy ";
+            String eighty = "Eighty ";
+            String ninety = "Ninety ";
+            String hundered = " Hundered ";
+            String teen = "teen";
+            String result = " ";
+
+            int m, n, i, j;
+            m = a / 10;
+            i = m / 10;
+            n = a % 10;
+            j = m % 10;
+            flag = true;
+            if (a >= 0 && a < 10) {
+                switch (a) {
+                    case 0:
+                        result = zero;
+                        break;
                     case 1:
-                        result = "One";
+                        result = one;
                         break;
                     case 2:
-                        result = "Two";
+                        result = two;
                         break;
                     case 3:
-                        result = "Three";
+                        result = three;
                         break;
                     case 4:
-                        result = "Four";
+                        result = four;
                         break;
                     case 5:
-                        result = "Five";
+                        result = five;
                         break;
                     case 6:
-                        result = "Six";
+                        result = six;
                         break;
                     case 7:
-                        result = " Seven";
+                        result = seven;
                         break;
                     case 8:
-                        result = "Eight";
+                        result = eight;
                         break;
                     case 9:
-                        result = "Nine";
-                        break;
-                    case 10:
-                        result = " Ten";
-                        break;
+                        result = nine;
                 }
-            } else if (number < 20) {
-                switch (number) {
-
+                System.out.print(result);
+            } else if (a < 20) {
+                switch (a) {
+                    case 10:
+                        result = ten;
+                        break;
                     case 11:
-                        result = " Eleven";
+                        result = "Eleven";
                         break;
                     case 12:
                         result = "Twelve";
                         break;
                     case 13:
-                        result = " Three " + teen;
+                        result = "Thir" + teen;
                         break;
                     case 14:
-                        result = " Four " + teen;
+                        result = four + teen;
                         break;
                     case 15:
-                        result = " Five " + teen;
+                        result = "Fir" + teen;
                         break;
                     case 16:
-                        result = " Six " + teen;
+                        result = six + teen;
                         break;
                     case 17:
-                        result = " Seven " + teen;
+                        result = seven + teen;
                         break;
                     case 18:
-                        result = " Eight " + teen;
+                        result = eight + teen;
                         break;
                     case 19:
-                        result = " Nine " + teen;
+                        result = nine + teen;
+                }
+                System.out.print(result);
+            } else if (a < 100) {
+                switch (m) {
+                    case 2:
+                        result = twenty;
                         break;
+                    case 3:
+                        result = thirty;
+                        break;
+                    case 4:
+                        result = forty;
+                        break;
+                    case 5:
+                        result = fifty;
+                        break;
+                    case 6:
+                        result = sixty;
+                        break;
+                    case 7:
+                        result = seventy;
+                        break;
+                    case 8:
+                        result = eighty;
+                        break;
+                    case 9:
+                        result = ninety;
                 }
-            } else if (number < 100) {
-                switch (number){
-
+                System.out.print(result);
+                switch (n) {
+                    case 1:
+                        result = one;
+                        break;
+                    case 2:
+                        result = two;
+                        break;
+                    case 3:
+                        result = three;
+                        break;
+                    case 4:
+                        result = four;
+                        break;
+                    case 5:
+                        result = five;
+                        break;
+                    case 6:
+                        result = six;
+                        break;
+                    case 7:
+                        result = seven;
+                        break;
+                    case 8:
+                        result = eight;
+                        break;
+                    case 9:
+                        result = nine;
                 }
+                System.out.print(result);
+            } else if (a < 1000) {
+                switch (i) {
+                    case 1:
+                        result = one;
+                        break;
+                    case 2:
+                        result = two;
+                        break;
+                    case 3:
+                        result = three;
+                        break;
+                    case 4:
+                        result = four;
+                        break;
+                    case 5:
+                        result = five;
+                        break;
+                    case 6:
+                        result = six;
+                        break;
+                    case 7:
+                        result = seven;
+                        break;
+                    case 8:
+                        result = eight;
+                        break;
+                    case 9:
+                        result = nine;
+                }
+                System.out.print(result + " " + hundered);
+                switch (j) {
+                    case 2:
+                        result = twenty;
+                        break;
+                    case 3:
+                        result = thirty;
+                        break;
+                    case 4:
+                        result = forty;
+                        break;
+                    case 5:
+                        result = fifty;
+                        break;
+                    case 6:
+                        result = sixty;
+                        break;
+                    case 7:
+                        result = seventy;
+                        break;
+                    case 8:
+                        result = eighty;
+                        break;
+                    case 9:
+                        result = ninety;
+                }
+                System.out.print(result);
+                switch (n) {
+                    case 1:
+                        result = one;
+                        break;
+                    case 2:
+                        result = two;
+                        break;
+                    case 3:
+                        result = three;
+                        break;
+                    case 4:
+                        result = four;
+                        break;
+                    case 5:
+                        result = five;
+                        break;
+                    case 6:
+                        result = six;
+                        break;
+                    case 7:
+                        result = seven;
+                        break;
+                    case 8:
+                        result = eight;
+                        break;
+                    case 9:
+                        result = nine;
+                }
+                System.out.println(result);
+            } else {
+                System.out.println("Số lớn quá, 3 số thôi");
             }
-        }
-
-
-        System.out.println(result);
+            System.out.println("\n false để nhập lại.\n true để thoát.");
+            boolean reset = sc.nextBoolean();
+            if (reset) {
+                flag = true;
+            } else {
+                flag = false;
+            }
+        } while (!flag);
     }
 }
