7f742bd76262861ddc975cba17caf90ec0c32485
Khanh280 <Khanh280>
2023-03-07 16:54:50 +0700

    Bai tap

--- module02/src/loop_java/DisplayFirstPrimes.java
+++ module02/src/loop_java/DisplayFirstPrimes.java
@@ -10,21 +10,19 @@ public class DisplayFirstPrimes {
         int count;
         int n = 0;
         System.out.println(numbers + " số nguyên tố đầu tiên là: ");
-        while (n < numbers) {
-            for (int i = 1; i > 0; i++) {
-                count = 0;
-                for (int j = 1; j <= i; j++) {
-                    if (i % j == 0) {
-                        count++;
-                    }
+        for (int i = 1; i > 0; i++) {
+            count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
                 }
-                if (count == 2) {
-                    System.out.println(i);
-                    n++;
-                }
-                if (n == numbers) {
-                    break;
-                }
+            }
+            if (count == 2) {
+                System.out.println(i);
+                n++;
+            }
+            if (n == numbers) {
+                break;
             }
         }
     }
