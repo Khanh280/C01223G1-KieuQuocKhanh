1c6c8d49fb7eb710091692d5608b17ee4462255c
Khanh280 <Khanh280>
2023-03-22 12:53:48 +0700

    Fix

--- module02/src/thuat_toan_tim_kiem/StringHaveLength.java
+++ module02/src/thuat_toan_tim_kiem/StringHaveLength.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class StringHaveLength {
 
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập Tìm chuỗi tăng dần có độ dài lớn nhất: ");
@@ -15,7 +14,6 @@ public class StringHaveLength {
         for (int i = 0; i < chars.length; i++) {
             String result = "";
             int max = 0;
-            int index = 0;
             for (int j = i; j < chars.length; j++) {
                 if (max <= chars[j]) {
                     max = chars[j];
