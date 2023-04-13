7dde1f9403bce63f6e7d9d4907623b1c13036608
Khanh280 <Khanh280>
2023-04-13 13:22:12 +0700

    FIX

--- module03/thiet_ke_csdl/sales_manager.sql
+++ module03/thiet_ke_csdl/sales_manager.sql
@@ -2,22 +2,23 @@ CREATE DATABASE sales_manager;
 USE sales_manager;
 CREATE TABLE customers(
 customer_id INT PRIMARY KEY,
-customer_name VARCHAR(50),
-sustomer_age INT
+customer_name VARCHAR(50) NOT NULL,
+sustomer_age INT NOT NULL,
+CHECK (sustomer_age > 0)
 );
 
 CREATE TABLE `order`(
 	order_id INT PRIMARY KEY,
     customer_id INT,
     FOREIGN KEY(customer_id) REFERENCES customers(customer_id),
-    order_date DATE,
-    order_total_price DOUBLE
+    order_date DATE NOT NULL,
+    order_total_price DOUBLE NOT NULL
 ); 
 
 CREATE TABLE product(
 product_id INT PRIMARY KEY,
-product_name VARCHAR(50),
-product_price FLOAT
+product_name VARCHAR(50) NOT NULL,
+product_price FLOAT NOT NULL
 );
 
 CREATE TABLE order_detail(
@@ -26,5 +27,5 @@ product_id INT,
 PRIMARY KEY(order_id,product_id),
 FOREIGN KEY(order_id) REFERENCES `order`(order_id),
 FOREIGN KEY(product_id) REFERENCES product(product_id),
-order_detail_qty VARCHAR(50)
+order_detail_qty VARCHAR(50) NOT NULL
 );
--- module03/thiet_ke_csdl/table_relational_db.sql
+++ module03/thiet_ke_csdl/table_relational_db.sql
@@ -2,42 +2,41 @@ CREATE DATABASE quan_ly_don_hang;
 use quan_ly_don_hang;
 CREATE TABLE nha_cung_cap(
 ma_nha_cung_cap INT PRIMARY KEY,
-ten_nha_cung_cap VARCHAR(50),
-dia_chi VARCHAR(50)
+ten_nha_cung_cap VARCHAR(50)  NOT NULL,
+dia_chi VARCHAR(50) NOT NULL
 );
 CREATE TABLE so_dien_thoai(
-ma_so_dien_thoai INT PRIMARY KEY,
-so_dien_thoai VARCHAR(50),
-ma_nha_cung_cap INT,
+so_dien_thoai VARCHAR(11) NOT NULL,
+ma_nha_cung_cap INT NOT NULL,
 FOREIGN KEY (ma_nha_cung_cap) REFERENCES nha_cung_cap(ma_nha_cung_cap)
 );
 CREATE TABLE phieu_xuat(
-ma_phieu_xuat INT PRIMARY KEY,
-ngay_xuat DATE
+ma_phieu_xuat INT PRIMARY KEY ,
+ngay_xuat DATE NOT NULL
 );
 
 CREATE TABLE phieu_nhap(
 ma_phieu_nhap INT PRIMARY KEY,
-ngay_nhap DATE
+ngay_nhap DATE NOT NULL
 );
 
 CREATE TABLE vat_tu(
 ma_vat_tu INT PRIMARY KEY,
-ten_vat_tu VARCHAR(50)
+ten_vat_tu VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE don_dat_hang (
 ma_don_hang INT PRIMARY KEY,
-ngay_dat_hang DATE,
-ma_nha_cung_cap INT,
+ngay_dat_hang DATE NOT NULL,
+ma_nha_cung_cap INT NOT NULL,
 FOREIGN KEY (ma_nha_cung_cap) REFERENCES nha_cung_cap(ma_nha_cung_cap)
 );
 
 CREATE TABLE phieu_xuat_vat_tu(
 ma_phieu_xuat INT,
 ma_vat_tu INT,
-don_gia_xuat INT,
-so_luong_xuat INT,
+don_gia_xuat FLOAT NOT NULL,
+so_luong_xuat INT NOT NULL,
 PRIMARY KEY(ma_phieu_xuat,ma_vat_tu),
 FOREIGN KEY (ma_phieu_xuat) REFERENCES phieu_xuat(ma_phieu_xuat),
 FOREIGN KEY (ma_vat_tu) REFERENCES vat_tu(ma_vat_tu)
@@ -46,8 +45,8 @@ FOREIGN KEY (ma_vat_tu) REFERENCES vat_tu(ma_vat_tu)
 CREATE TABLE phieu_nhap_vat_tu(
 ma_phieu_nhap INT,
 ma_vat_tu INT,
-don_gia_nhap INT,
-so_luong_nhap INT,
+don_gia_nhap FLOAT NOT NULL,
+so_luong_nhap INT NOT NULL,
 PRIMARY KEY(ma_phieu_nhap,ma_vat_tu),
 FOREIGN KEY (ma_phieu_nhap) REFERENCES phieu_nhap(ma_phieu_nhap),
 FOREIGN KEY (ma_vat_tu) REFERENCES vat_tu(ma_vat_tu)
