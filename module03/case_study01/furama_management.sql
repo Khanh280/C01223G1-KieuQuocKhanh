3cdf03038a7ba5e5cf54a8719fa336100fd25912
Khanh280 <Khanh280>
2023-04-15 18:43:00 +0700

    case study

--- module03/case_study01/furama_management.sql
+++ module03/case_study01/furama_management.sql
@@ -262,20 +262,79 @@ INNER JOIN hop_dong_chi_tiet AS hdct ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu
 INNER JOIN hop_dong AS hd ON hdct.ma_hop_dong = hd.ma_hop_dong
 INNER JOIN khach_hang AS kh ON hd.ma_khach_hang = kh.ma_khach_hang
 INNER JOIN loai_khach AS lk ON kh.ma_loai_khach = lk.ma_loai_khach
-WHERE lk.ten_loai_khach = "Diamond" AND (kh.dia_chi LIKE "%Vinh%" OR kh.dia_chi LIKE "%Quảng Ngãi%")
+WHERE lk.ten_loai_khach = "Diamond" AND (kh.dia_chi LIKE "%Vinh%" OR kh.dia_chi LIKE "%Quảng Ngãi%");
 
 -- Task12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem 
 -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được khách 
 -- hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
 
+SELECT hd.ngay_lam_hop_dong,hd.ma_hop_dong, nv.ho_ten, kh.ho_ten, kh.so_dien_thoai, dv.ten_dich_vu,
+SUM(IFNULL(hdct.so_luong,0)) AS so_luong_dich_vu_di_kem, hd.tien_dat_coc
+FROM hop_dong AS hd
+INNER JOIN nhan_vien AS nv ON hd.ma_nhan_vien = nv.ma_nhan_vien
+INNER JOIN khach_hang AS kh ON hd.ma_khach_hang = kh.ma_khach_hang
+INNER JOIN dich_vu AS dv ON hd.ma_dich_vu = dv.ma_dich_vu
+LEFT JOIN hop_dong_chi_tiet AS hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+LEFT JOIN dich_vu_di_kem AS dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+WHERE
+hd.ma_dich_vu IN (
+SELECT hd.ma_dich_vu
+FROM dich_vu AS dv
+INNER JOIN hop_dong AS hd ON dv.ma_dich_vu = hd.ma_dich_vu
+WHERE year(hd.ngay_lam_hop_dong) = 2020 AND quarter(hd.ngay_lam_hop_dong) = 4)
+AND
+ hd.ma_dich_vu NOT IN (
+SELECT hd.ma_dich_vu
+FROM dich_vu AS dv
+INNER JOIN hop_dong AS hd ON dv.ma_dich_vu = hd.ma_dich_vu
+WHERE year(hd.ngay_lam_hop_dong) = 2021 AND (quarter(hd.ngay_lam_hop_dong) = 1 OR quarter(hd.ngay_lam_hop_dong) =2)
+)
+ GROUP BY hd.ma_hop_dong
+;
+
 -- Task13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+SELECT dvdk.ma_dich_vu_di_kem,dvdk.ten_dich_vu_di_kem, SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem
+FROM dich_vu_di_kem AS dvdk
+INNER JOIN hop_dong_chi_tiet AS hdct ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+GROUP BY hdct.ma_dich_vu_di_kem
+HAVING so_luong_dich_vu_di_kem = (
+SELECT SUM(hdct.so_luong) AS tong_so_luong
+FROM dich_vu_di_kem AS dvdk
+INNER JOIN hop_dong_chi_tiet AS hdct ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+GROUP BY hdct.ma_dich_vu_di_kem
+ORDER BY tong_so_luong DESC LIMIT 1
+);
 
 -- Task14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, 
 -- so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+-- SELECT hd.ma_hop_dong, ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_di_kem, COUNT(hdct.so_luong) AS so_lan_su_dung;
 
--- Task15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+SELECT hdct.ma_hop_dong, ldv.ten_loai_dich_vu , dvdk.ten_dich_vu_di_kem,COUNT(hdct.ma_dich_vu_di_kem) AS so_lan_su_dung
+FROM hop_dong AS hd
+RIGHT JOIN hop_dong_chi_tiet AS hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+INNER JOIN dich_vu_di_kem AS dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+INNER JOIN dich_vu AS dv ON hd.ma_dich_vu = dv.ma_dich_vu
+INNER JOIN loai_dich_vu AS ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+GROUP BY hdct.ma_dich_vu_di_kem,hdct.ma_hop_dong
+HAVING dvdk.ten_dich_vu_di_kem IN (
+SELECT dvdk.ten_dich_vu_di_kem
+FROM dich_vu_di_kem AS dvdk
+INNER JOIN hop_dong_chi_tiet AS hdct ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+GROUP BY hdct.ma_dich_vu_di_kem
+HAVING COUNT(*) =1); -- count(*) = 1 sẽ đếm hdct.ma_dich_vu_di_kem bên phía hop_dong_chi_tiet nếu chỉ xuất hiện 1 lần thì sẽ trả về 
 
 
+-- Task15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan,
+-- so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+
+SELECT nv.ma_nhan_vien, nv.ho_ten, td.ten_trinh_do, bp.ten_bo_phan, nv.so_dien_thoai, nv.dia_chi
+FROM nhan_vien AS nv
+INNER JOIN hop_dong AS hd ON nv.ma_nhan_vien = hd.ma_nhan_vien
+INNER JOIN bo_phan AS bp ON nv.ma_bo_phan = bp.ma_bo_phan
+INNER JOIN trinh_do AS td ON nv.ma_trinh_do = td.ma_trinh_do
+WHERE (year(hd.ngay_lam_hop_dong ) BETWEEN 2020 AND 2021)
+GROUP BY hd.ma_nhan_vien 
+HAVING COUNT(hd.ma_nhan_vien) <=3  
 
 
 
